7d645451e3c15fe0f0fc1d6cfd1846bdd592572b
timothymureithi <timothy.mureithi@student.moringaschool.com>
2022-09-03 20:04:31 +0300

    Done

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Add your Circle class here
+let pi = Math.PI;
+
+class Circle{
+    constructor(radius){
+        this.radius = radius;
+    }
+
+    get diameter(){
+        return this.radius * 2;
+    }
+
+    set diameter(x){
+        this.radius = x / 2;
+      }
+
+    get circumference(){
+        return (this.radius * 2) * pi;
+    }
+
+    set circumference(x){
+        this.radius = x / (pi * 2);
+    }
+
+    get area () {
+        return Math.PI * this.radius * this.radius;
+    }
+
+    set area (area) {
+        this.radius = Math.sqrt(area / Math.PI);
+    }
+}
